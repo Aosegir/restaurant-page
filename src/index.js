cb6c11f2f2c8a7d109ec4dbe6306d56bcfbe7ce4
Aosegir <128326345+Aosegir@users.noreply.github.com>
2024-05-19 20:27:39 -0700

    Updated JS and HTML to allow for button functionality.

--- src/index.js
+++ src/index.js
@@ -1,25 +1,82 @@
+// IMPORTS
 import './style.css';
 import Autumn from "../images/autumn.jpg";
 
-// content is the scope we will be building the website page within
+//DOM VARIABLES
+let homeButton = document.getElementById('home');
+let menuButton = document.getElementById('menu');
+let aboutButton = document.getElementById('about');
+
+// content is the scope we will be building the website pages within
 let content = document.getElementById('content');
 
-// BUILDING HEADER TEXT
-let headerText = document.createElement('h1');
-headerText.textContent = "The Autumn Night";
-content.appendChild(headerText);
+/*
+    HOME PAGE LOADER
+*/
+function loadHome() {
+    while(content.lastChild) {
+        content.removeChild(content.lastChild);
+    }
 
-// BUILDING HEADER IMAGE
-let headerImage = new Image();
-headerImage.src = Autumn;
-content.appendChild(headerImage);
+    // BUILDING HEADER TEXT
+    let headerText = document.createElement('h1');
+    headerText.textContent = "The Autumn Night";
+    content.appendChild(headerText);
 
-// BUILDING SUBHEADER
-let subHeader = document.createElement('h4');
-subHeader.textContent = "Welcome to The Autumn Night!";
-content.appendChild(subHeader);
+    // BUILDING HEADER IMAGE
+    let headerImage = new Image();
+    headerImage.src = Autumn;
+    content.appendChild(headerImage);
 
-// BUILDING TEXT BLURB
-let text = document.createElement('p');
-text.textContent = "This restaurant seeks to bring the wonders of the season right to your doorstep. Please feel free to browse our selection until you find something that matches your appetites. You can be sure that you'll never be disappointed!"
-content.appendChild(text);
+    // BUILDING SUBHEADER
+    let subHeader = document.createElement('h4');
+    subHeader.textContent = "Welcome to The Autumn Night!";
+    content.appendChild(subHeader);
+
+    // BUILDING TEXT BLURB
+    let text = document.createElement('p');
+    text.textContent = "This restaurant seeks to bring the wonders of the season right to your doorstep. Please feel free to browse our selection until you find something that matches your appetites. You can be sure that you'll never be disappointed!"
+    content.appendChild(text);
+}
+
+/*
+    MENU PAGE LOADER
+*/
+function loadMenu() {
+    while(content.lastChild) {
+        content.removeChild(content.lastChild);
+    }
+
+    // BUILDING HEADER TEXT
+    let headerText = document.createElement('h1');
+    headerText.textContent = "The Autumn Night";
+    content.appendChild(headerText);
+}
+
+/*
+    ABOUT PAGE LOADER
+*/
+function loadAbout() {
+    while(content.lastChild) {
+        content.removeChild(content.lastChild);
+    }
+
+    // BUILDING SUBHEADER
+    let subHeader = document.createElement('h4');
+    subHeader.textContent = "Welcome to The Autumn Night!";
+    content.appendChild(subHeader);
+}
+
+loadHome();
+
+homeButton.addEventListener('click', () => {
+    loadHome();
+});
+
+menuButton.addEventListener('click', () => {
+    loadMenu();
+});
+
+aboutButton.addEventListener('click', () => {
+    loadAbout();
+});
