0141a2a31e1d3724a7124b4459732dbd8e96d89e
Aosegir <128326345+Aosegir@users.noreply.github.com>
2024-05-20 19:51:26 -0700

    Updated HTML and CSS to improve UI for main page.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import Pizza from "../images/pizza.jpeg";
 //DOM VARIABLES
 let homeButton = document.getElementById('home');
 let menuButton = document.getElementById('menu');
+let specialsButton = document.getElementById('specials');
 let aboutButton = document.getElementById('about');
 
 // content is the scope we will be building the website pages within
@@ -16,32 +17,76 @@ let content = document.getElementById('content');
 
 
 
-
-
+/*
+    CLEAR PAGE FUNCTION
+*/
+function clearPage() {
+    while(content.lastChild) {
+        content.removeChild(content.lastChild);
+    };
+};
 
 /*
     HOME PAGE LOADER
 */
 function loadHome() {
-    while(content.lastChild) {
-        content.removeChild(content.lastChild);
-    }
+    /*
+        div to contain everything
+            subdiv to contain image on left 3rd of screen
+                image  (pizza slice)
+            subdiv to contain history text on right 2/3rds of screen
+                title header
+                lorem ipsum history 
+    */
 
+    //DIV BUILDERS
+    let contentDiv = document.createElement('div');
+    let leftDiv = document.createElement('div');
+    let rightDiv = document.createElement('div');
+
+    //LEFT DIV
+    leftDiv.classList.add('flex');
+    leftDiv.classList.add('single');
+    let homeImage = new Image();
+    homeImage.src = Pizza;
+    leftDiv.appendChild(homeImage);
+
+    //RIGHT DIV
+    rightDiv.classList.add('flex');
+    rightDiv.classList.add('column');
+    rightDiv.classList.add('main-center');
+    rightDiv.classList.add('cross-center');
+    rightDiv.classList.add('double');
+    let homeHeader = document.createElement('h1');
+    homeHeader.textContent = 'Welcome to The Hot Spot!';
+    rightDiv.appendChild(homeHeader);
+
+    let history = document.createElement('p');
+    history.textContent = "This is a brief overview of the history of the Hot Spot, from our founder's fight in the War of 1812 to our revolutionary discovery of ancient dough-kneading techniques recovered from a pharoah's tomb by my Great-Aunt Birget.";
+    rightDiv.appendChild(history);
+
+    //CONTENT DIV
+    contentDiv.classList.add('flex');
+    contentDiv.appendChild(leftDiv);
+    contentDiv.appendChild(rightDiv);
+
+    content.appendChild(contentDiv);
+}
+
+/*
+    MENU PAGE LOADER
+*/
+function loadMenu() {
     // BUILDING HEADER TEXT
     let headerText = document.createElement('h1');
     headerText.textContent = "The Hot Spot";
     content.appendChild(headerText);
+}
 
-    // BUILDING HEADER IMAGE
-    let headerImage = new Image();
-    headerImage.src = Pizza;
-    content.appendChild(headerImage);
-
-    // BUILDING SUBHEADER
-    let subHeader = document.createElement('h4');
-    subHeader.textContent = "Welcome to The Hot Spot!";
-    content.appendChild(subHeader);
-
+/*
+    SPECIALS PAGE LOADER
+*/
+function loadSpecials() {
     // BUILDING TEXT BLURB
     let text = document.createElement('p');
     text.textContent = "Text"
@@ -49,27 +94,9 @@ function loadHome() {
 }
 
 /*
-    MENU PAGE LOADER
-*/
-function loadMenu() {
-    while(content.lastChild) {
-        content.removeChild(content.lastChild);
-    }
-
-    // BUILDING HEADER TEXT
-    let headerText = document.createElement('h1');
-    headerText.textContent = "The Hot Spot";
-    content.appendChild(headerText);
-}
-
-/*
     ABOUT PAGE LOADER
 */
 function loadAbout() {
-    while(content.lastChild) {
-        content.removeChild(content.lastChild);
-    }
-
     // BUILDING SUBHEADER
     let subHeader = document.createElement('h4');
     subHeader.textContent = "Welcome to The Hot Spot!";
@@ -79,13 +106,21 @@ function loadAbout() {
 loadHome();
 
 homeButton.addEventListener('click', () => {
+    clearPage();
     loadHome();
 });
 
 menuButton.addEventListener('click', () => {
+    clearPage();
     loadMenu();
 });
 
+specialsButton.addEventListener('click', () => {
+    clearPage();
+    loadSpecials();
+});
+
 aboutButton.addEventListener('click', () => {
+    clearPage();
     loadAbout();
 });
